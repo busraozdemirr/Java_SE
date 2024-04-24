a1f8d85bd7c82b03551385cc382039b5eff899a7
busraozdemirr <busraozdemir4444@gmail.com>
2024-04-24 17:03:44 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/week04/functionel_interface/interface01.java
@@ -0,0 +1,9 @@
+package week04.functionel_interface;
+
+public class interface01 {
+    public interface Interface01{
+
+        public void abstractMethod();//1 tane abstract method olmalı
+        //public void abstractMethod2(); -> 2 taneye izin yok - MİN 1 MAX 1 METHOD.
+    }
+}
